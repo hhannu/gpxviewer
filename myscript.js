dadc9d44b3c7ca53afa2ea1b70faeabcc4174282
Hannu H <hth@bowmore..example.com>
2014-12-18 20:57:08 +0200

    Center and zoom map correctly.

--- myscript.js
+++ myscript.js
@@ -15,19 +15,28 @@ function handleFile(files) {
                 var parser = new DOMParser();
                 var xmlDoc = parser.parseFromString(reader.result, "text/xml");
                 var points = xmlDoc.getElementsByTagName("trkpt");
-                for (i = 0; i < points.length; i++) {
-                    trackCoordinates.push(new google.maps.LatLng(points[i].getAttribute("lat"), points[i].getAttribute("lon")));
-                }
+                var bounds = new google.maps.LatLngBounds();
                 
+                // get all trackpoints from gpx file
+                for (i = 0; i < points.length; i++) {
+                    var trackPoint = new google.maps.LatLng(points[i].getAttribute("lat"), points[i].getAttribute("lon"));
+                    trackCoordinates.push(trackPoint);
+                    bounds.extend(trackPoint);
+                }
+                                
+                // options for map
                 var mapOptions = {
-                    zoom: 11,
-                    center: new google.maps.LatLng(points[0].getAttribute("lat"), points[0].getAttribute("lon")),
+                    zoom: 0,
+                    center: bounds.getCenter(),
                     mapTypeId: google.maps.MapTypeId.TERRAIN,
-                    streetViewControl: false
+                    streetViewControl: false,
+                    noClear: false
                 };
 
+                // the map
                 var map = new google.maps.Map(document.getElementById('myMap'), mapOptions);
 
+                // line to be drawn on map
                 var route = new google.maps.Polyline({
                     path: trackCoordinates,
                     strokeColor: '#FF0000',
@@ -35,19 +44,24 @@ function handleFile(files) {
                     strokeWeight: 2
                 });
 
+                // draw route, center map and adjust zoom
                 route.setMap(map);
+                map.fitBounds(bounds);
                 
+                // marker for start position
                 var startM = new google.maps.Marker({
                     position: trackCoordinates[0],
                     title: "Start",
                     map: map
                 });
                 
+                // marker for end position
                 var stopM = new google.maps.Marker({
                     position: trackCoordinates[trackCoordinates.length - 1],
                     title: "Start",
                     map: map
                 });
+                
                 document.getElementById("info").innerHTML =  "";
             }            
         }
